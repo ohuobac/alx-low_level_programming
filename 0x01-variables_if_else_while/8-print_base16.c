de620a7e73daf99dd440bfe46276909610f5961b
Ohuoba Chika <ohuobac@gmail.com>
2022-09-09 19:03:02 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
