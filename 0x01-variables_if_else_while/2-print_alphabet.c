f43b3912d9160a52597a51f25bac9b4b743ff083
Ohuoba Chika <ohuobac@gmail.com>
2022-09-09 17:34:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	putchar(a);
+	a++;
+	putchar('\n');
+	return (0);
+}
