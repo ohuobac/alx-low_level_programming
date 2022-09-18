fa5de88353419cdfb80738374ffd4fe8ede9e8b5
Ohuoba Chika <ohuobac@gmail.com>
2022-09-18 10:20:41 -0700

    main.h edit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,7 +14,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void num_to_char(unsigned int);
+void print_integer(int m);
 void print_number(int n);
 
 #endif /* MAIN_H */
