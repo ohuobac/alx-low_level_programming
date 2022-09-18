45940341ef40ea1a87446fe90a37ee241523a18f
Ohuoba Chika <ohuobac@gmail.com>
2022-09-18 10:50:05 -0700

    101-print_number.c and main.h edits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,7 +14,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_integer(int m);
+void num_to_char(unsigned int);
 void print_number(int n);
 
 #endif /* MAIN_H */
