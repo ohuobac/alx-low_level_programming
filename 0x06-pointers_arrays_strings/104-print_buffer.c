6dea7d1fab4ad9bfd9e9634042af90f880edf747
Ohuoba Chika <ohuobac@gmail.com>
2022-09-23 03:07:40 -0700

    104-print_buffer.c   debugging

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,10 +12,10 @@ void print_line(char *c, int s, int l)
 {
 	int j, k;
 
-	for  (j = 0; j <= 9; j++)
+	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
-			printf("%02x", c[1 * 10 + j]);
+			printf("%02x", c[l * 10 + j]);
 		else
 			printf(" ");
 		if (j % 2)
@@ -23,8 +23,8 @@ void print_line(char *c, int s, int l)
 	}
 	for (k = 0; k <= s; k++)
 	{
-		if (c[1 * 10 + k] > 31 && c[1 * 10 + k] < 127)
-			putchar(c[1 * 10 + k]);
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
 		else
 			putchar('.');
 	}
