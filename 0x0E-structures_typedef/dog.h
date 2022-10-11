046dd8522355cd39ca1e5d6c5ba254a5805dd0c6
Ohuoba Chika <ohuobac@gmail.com>
2022-10-11 11:41:40 -0700

    README.md, dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - contains dog information
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Description: hold all data needed for dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
